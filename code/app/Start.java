61fdc796792a34b6b6584c5e662a7f9dcd031832
Voragun <voragun2543@gmail.com>
2023-05-25 16:31:53 +0700

    print another class

--- code/app/Start.java
+++ code/app/Start.java
@@ -1,7 +1,29 @@
 package app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Indexed;
+import org.springframework.stereotype.Service;
+
 class Start {
     public static void main(String[] args) {
-        System.out.println("hello");
+        ApplicationContext context;
+        context = SpringApplication.run(Setup.class);
+        
+        CheckLogIn c = context.getBean(CheckLogIn.class);
+        System.out.println(c.name);
+        
+        SpringApplication.exit(context);
     }
+}
+
+@SpringBootApplication
+class Setup {
+    
+}
+
+@Service @Indexed
+class CheckLogIn {
+    String name = "iCoffee";
 }
